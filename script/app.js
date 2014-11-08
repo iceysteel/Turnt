7a3bb06d4a91636f3a08985e6249ee9644e6ef91
Muhammad Rizky Wellyanto <wellyanto.rizky@gmail.com>
2014-11-08 14:16:34 -0800

    routing works. porting javascript files to angular. polishing the partials

--- script/app.js
+++ script/app.js
@@ -30,7 +30,7 @@ angular.module('app', ['ngRoute'])
             })
             .when('/parties', {
                 templateUrl: 'partials/parties.html',
-                controller: 'partiesController'
+                controller: 'partyController'
             })
             .otherwise({redirectTo:'/'})
     }])
@@ -49,24 +49,55 @@ angular.module('app', ['ngRoute'])
 
     }])
     .factory('partyFactory', ['$http', function ($http) {
-        return {
-            getParties: function () {
-                return $http.get('json/parties.json').then(function (response) {
-                    if (response.data.error) {
-                        return null;
-                    }
 
-                    return response.data;
-                })
-            }
-        };
+    }])
+    .factory('loginFactory', ['$http', function ($http) {
+
     }])
 
 /**
  * controllers
  */
     .controller('homeController', ['$scope', 'homeFactory', function ($scope, homeFactory) {
+        Parse.initialize("jvpZoE593I9616TtmoVX9qMzs9DYXVlfa6rudZ6P", "v1ySlxINSYW6OL1aKo7YZV7i4SjvG4D7Th2fhhWU");
+        var currentUser = Parse.User.current();
+        if (currentUser)
+        {
 
+        }
+        else
+        {
+            window.location = "login.html";
+        }
+
+        $scope.hostEvent = function () {
+            var Event = Parse.Object.extend("Parties");
+            var party = new Event();
+            var user = Parse.User.current();
+            user.fetch().then(function(fetchedUser){
+                var name = fetchedUser.getUsername();
+            }, function(error){
+                //Handle the error
+            });
+
+            party.set("Location", $scope.loc);
+            party.set("Year", $scope.yr);
+            party.set("Month", $scope.mo);
+            party.set("Day", $scope.day);
+            party.set("Cost",  $scope.co);
+            party.set("Owner", name);
+            party.save(null, {
+                success: function(party) {
+                    // Execute any logic that should take place after the object is saved.
+                    alert('New object created with objectId: ' + party.id);
+                },
+                error: function(party, error) {
+                    // Execute any logic that should take place if the save fails.
+                    // error is a Parse.Error with an error code and message.
+                    alert('Failed to create new object, with error code: ' + error.message);
+                }
+            });
+        }
     }])
     .controller('hostController', ['$scope', 'hostFactory', function ($scope, hostFactory) {
 
@@ -76,8 +107,40 @@ angular.module('app', ['ngRoute'])
     }])
     .controller('partyController', ['$scope', 'partyFactory', function ($scope, partyFactory) {
         $scope.parties = [];
-        partyFactory.getParties().then(function (data) {
-            angular.copy(data, $scope.parties);
-        });
+    }])
+    .controller('loginController', ['$scope', 'loginFactory', '$location', function ($scope, loginFactory, $location) {
+        $scope.signIn = function () {
+            Parse.initialize("jvpZoE593I9616TtmoVX9qMzs9DYXVlfa6rudZ6P", "v1ySlxINSYW6OL1aKo7YZV7i4SjvG4D7Th2fhhWU");
+            var user = new Parse.User();
+            Parse.User.logIn($scope.uname, $scope.password, {
+                success: function(user) {
+                    $location.path('/home');
+                },
+                error: function(user, error) {
+                    alert("Error: " + error.code + " " + error.message);
+                }
+            });
+        }
+    }])
+    .controller('registerController', ['$scope', 'registerFactory', '$location', function ($scope, registerFactory, $location) {
+        $scope.register = function () {
+            Parse.initialize("jvpZoE593I9616TtmoVX9qMzs9DYXVlfa6rudZ6P", "v1ySlxINSYW6OL1aKo7YZV7i4SjvG4D7Th2fhhWU");
+            var user = new Parse.User();
+            user.set("username", $scope.uname);
+            user.set("password", $scope.password);
+            user.set("email", scope.email);
+
+            // other fields can be set just like with Parse.Object
+
+            user.signUp(null, {
+                success: function(user) {
+                    $location.path('/home')
+                },
+                error: function(user, error) {
+                    // Show the error message somewhere and let the user try again.
+                    alert("Error: " + error.code + " " + error.message);
+                }
+            });
+        }
     }])
 ;
