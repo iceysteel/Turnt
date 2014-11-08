4fc2aa22df1eee8cf005105d1110d4a53090b92d
Muhammad Rizky Wellyanto <wellyanto.rizky@gmail.com>
2014-11-08 12:05:47 -0800

    Routing Works!!! still have minor errors.

--- script/app.js
+++ script/app.js
@@ -2,7 +2,7 @@
  * Created by MuhammadRizky on 11/8/2014.
  */
 
-angular.module('app', ['ngRoute','app.account','app.host','app.parties'])
+angular.module('app', ['ngRoute'])
 /**
  * config
  */
@@ -20,6 +20,19 @@ angular.module('app', ['ngRoute','app.account','app.host','app.parties'])
                 templateUrl:'partials/register.html',
                 controller:'registerController'
             })
+            .when('/host',{
+                templateUrl: 'partials/host.html',
+                controller: 'hostController'
+            })
+            .when('/account',{
+                templateUrl: 'partials/account.html',
+                controller: 'accountController'
+            })
+            .when('/parties', {
+                templateUrl: 'partials/parties.html',
+                controller: 'partiesController'
+            })
+            .otherwise({redirectTo:'/'})
     }])
 
 /**
@@ -29,9 +42,42 @@ angular.module('app', ['ngRoute','app.account','app.host','app.parties'])
 
     }])
 
+    .factory('hostFactory', ['$http', function ($http) {
+
+    }])
+    .factory('accountFactory', ['$http', function ($http) {
+
+    }])
+    .factory('partyFactory', ['$http', function ($http) {
+        return {
+            getParties: function () {
+                return $http.get('json/parties.json').then(function (response) {
+                    if (response.data.error) {
+                        return null;
+                    }
+
+                    return response.data;
+                })
+            }
+        };
+    }])
+
 /**
  * controllers
  */
     .controller('homeController', ['$scope', 'homeFactory', function ($scope, homeFactory) {
 
-    }]);
+    }])
+    .controller('hostController', ['$scope', 'hostFactory', function ($scope, hostFactory) {
+
+    }])
+    .controller('accountController', ['$scope', 'accountFactory', function ($scope, accountFactory) {
+
+    }])
+    .controller('partyController', ['$scope', 'partyFactory', function ($scope, partyFactory) {
+        $scope.parties = [];
+        partyFactory.getParties().then(function (data) {
+            angular.copy(data, $scope.parties);
+        });
+    }])
+;
