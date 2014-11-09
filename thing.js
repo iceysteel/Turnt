c82f5e012947eb553d6d5139ddff1fde45d7a380
Michael Erdey <michaelerdey@gmail.com>
2014-11-08 17:36:22 -0800

    party creation

--- thing.js
+++ thing.js
@@ -31,3 +31,43 @@ function signIn()
               }
             });
         }
+		
+function newEvent()
+        {
+            var Event = Parse.Object.extend("Parties");
+            var party = new Event();
+            var user = Parse.User.current()
+			var name = user.getUsername();
+            party.set("Location", document.getElementById('loc').value);
+            party.set("Year", Number(document.getElementById('yr').value));
+            party.set("Month", document.getElementById('mo').value);
+            party.set("Day", Number(document.getElementById('day').value));
+            party.set("Cost",  Number(document.getElementById('co').value));
+            party.set("Owner", name);
+            party.save(null, {
+              success: function(party) {
+                // Execute any logic that should take place after the object is saved.
+                alert('New object created with objectId: ' + party.id);
+              },
+              error: function(party, error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('Failed to create new object, with error code: ' + error.message);
+              }
+            });
+        }
+        Parse.initialize("jvpZoE593I9616TtmoVX9qMzs9DYXVlfa6rudZ6P", "v1ySlxINSYW6OL1aKo7YZV7i4SjvG4D7Th2fhhWU");
+        var currentUser = Parse.User.current();
+        if (currentUser) 
+        {
+            
+        } 
+        else 
+        {
+            window.location = "login.html";
+        }
+		
+		function isNumberKey(evt)
+        {
+          return (event.charCode >= 48 && event.charCode <= 57);
+		}
